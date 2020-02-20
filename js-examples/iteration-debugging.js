d91ddd3ae4a5a0f9c06647006c4a5e42e3ea29e5
Ben Spector <30628696+benspector3@users.noreply.github.com>
2020-02-20 12:47:07 -0600

    Update iteration-debugging.js

--- js-examples/iteration-debugging.js
+++ js-examples/iteration-debugging.js
@@ -1,11 +1,11 @@
 function iterationDebugging() {
+  debugger;
 
   var fruits = ["apples", "bananas", "cherries", "dates", "elderberries"];
   
-  debugger;
-  
   for (var i = 0; i < fruits.length; i++) {
+    
     console.log("I like " + fruits[i]);
-  }
   
+  }
 }
