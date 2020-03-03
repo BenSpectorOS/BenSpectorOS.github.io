f3eca2fd1ce219d7e9417cd923dffc3e12993cc4
benspector3 <ben@operationspark.org>
2020-03-03 12:08:46 -0600

    iteration example

--- js-examples/iteration-debugging.js
+++ js-examples/iteration-debugging.js
@@ -1,11 +1,22 @@
 function iterationDebugging() {
 
-  var fruits = ["apples", "bananas", "cherries", "dates", "elderberries"];
+  var fruits = [
+    {kind: 'apple', price: 0.5},
+    {kind: 'banana', price: 0.25},
+    {kind: 'raspberry', price: 4},
+    {kind: 'blueberry', price: 2.5},
+    {kind: 'strawberry', price: 3},
+    {kind: 'mango', price: 1},
+    {kind: 'pineapple', price: 2.5}
+  ];
   
   debugger;
   
   for (var i = 0; i < fruits.length; i++) {
-    console.log("I like " + fruits[i]);
+    var fruit = fruits[i];
+    var kind = fruit.kind;
+    var price = fruit.price;
+    console.log(`the price of one ${kind} is $${price}`);
   }
   
 }
